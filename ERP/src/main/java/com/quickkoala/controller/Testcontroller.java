9b35cd38295d7b990b6511322cc8827ff8285734
Donggeon0304 <rodzld18@naver.com>
2024-09-02 18:11:37 +0900

    koala db jpa 연결

--- ERP/src/main/java/com/quickkoala/controller/Testcontroller.java
+++ ERP/src/main/java/com/quickkoala/controller/Testcontroller.java
@@ -1,13 +1,29 @@
 package com.quickkoala.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.quickkoala.entity.TestEntity;
+import com.quickkoala.service.TestService;
 
 @Controller
 public class Testcontroller {
 
-	@GetMapping("test")
-	public String test() {
-		return "test";
-	}
+	@Autowired
+	private TestService testService;
+	
+	 @GetMapping("test")
+	 public String index(Model model) {
+	    List<String> items = Arrays.asList("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5","Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5","Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5","Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5");
+	    model.addAttribute("items", items);
+	    List<TestEntity> member = testService.getAllMember();
+	    model.addAttribute("member",member);
+	    return "test";
+	 }
+	 
 }
--- ERP/src/main/java/com/quickkoala/repository/TestRepository.java
+++ ERP/src/main/java/com/quickkoala/repository/TestRepository.java
@@ -1,5 +1,14 @@
 package com.quickkoala.repository;
 
-public class TestRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.quickkoala.entity.TestEntity;
+
+@Repository
+public interface TestRepository extends JpaRepository<TestEntity, Integer>{
+	List<TestEntity> findAll();
+	
 }
