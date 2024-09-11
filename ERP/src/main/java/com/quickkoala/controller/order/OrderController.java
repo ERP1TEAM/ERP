cfdd9157de10a7c951118080d9d0f40678b7fbe0
airmungs <airmungs@gmail.com>
2024-09-11 18:07:24 +0900

    commit

--- ERP/src/main/java/com/quickkoala/controller/order/OrderController.java
+++ ERP/src/main/java/com/quickkoala/controller/order/OrderController.java
@@ -2,11 +2,14 @@ package com.quickkoala.controller.order;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/distributor")
 public class OrderController {
-	@GetMapping("distributor/order.do")
+	@GetMapping("/order.do")
 	public String order() {
 		return "order/order";
 	}
+	
 }
