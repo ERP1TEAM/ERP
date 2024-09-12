e07b07082d4bbd606ecd6fd530c632d9c9377e0c
airmungs <airmungs@gmail.com>
2024-09-12 18:10:09 +0900

    0912

--- ERP/src/main/java/com/quickkoala/controller/sales/OrderController.java
+++ ERP/src/main/java/com/quickkoala/controller/sales/OrderController.java
@@ -7,9 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/sales")
 public class OrderController {
+	
 	@GetMapping("/order.do")
 	public String order() {
 		return "sales/order";
 	}
 	
+	@GetMapping("/main.do")
+	public String main() {
+		return "sales/main";
+	}
+	
+	@GetMapping("/orderlist.do")
+	public String orderlist() {
+		return "sales/orderlist";
+	}
 }
