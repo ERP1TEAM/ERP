8a4cce64e1bfe1f9165a378537fcc42cc4935b98
5ore5o <5ore5o@naver.com>
2024-09-23 15:59:22 +0900

    lot엔티티 storage_location 삭제한거

---
+++ ERP/src/main/java/com/quickkoala/entity/stock/LotEntity.java
@@ -0,0 +1,41 @@
+package com.quickkoala.entity.stock;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime; 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "lot")
+public class LotEntity {
+	@Id
+    @Column(name = "lot_number", nullable = false, length = 20)
+    private String lotNumber;
+
+    @Column(name = "product_code", nullable = false, length = 8)
+    private String productCode;
+
+    @Column(name = "supplier_code", nullable = false, length = 6)
+    private String supplierCode;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+    @Column(name = "production_date", nullable = false)
+    private LocalDate productionDate;
+
+    @Column(name = "receive_date", nullable = false)
+    private LocalDateTime receiveDate;
+
+    @Column(name = "last_update", nullable = false)
+    private LocalDateTime lastUpdate;
+}
