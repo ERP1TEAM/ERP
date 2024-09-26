71368f0815d0e673674e902b36fa483e9e32a8a0
Donggeon0304 <rodzld18@naver.com>
2024-09-26 17:38:43 +0900

    22

--- ERP/src/main/java/com/quickkoala/repository/supplier/ViewPurchaseSummaryRepository.java
+++ ERP/src/main/java/com/quickkoala/repository/supplier/ViewPurchaseSummaryRepository.java
@@ -29,17 +29,22 @@ public interface ViewPurchaseSummaryRepository extends JpaRepository<ViewPurchas
 	Page<ViewPurchaseSummaryEntity> findByTotalWtQuantityGreaterThanAndProductNameContainingOrderByOrderNumberDesc(
 	        int totalWtQuantity, String productName, Pageable pageable);
 	
-	@Query("SELECT v FROM ViewPurchaseSummaryEntity v WHERE " + "(:searchField IS NULL OR "
-			+ "(:codeType = '발주번호' AND LOWER(CONCAT(v.orderNumber)) LIKE LOWER(CONCAT('%', :searchField, '%'))) OR " + // 입고번호
-			"(:codeType = '상품명' AND LOWER(CONCAT(v.productName)) LIKE LOWER(CONCAT('%', :searchField, '%')))) AND " + // 상품명
-			"("
-	        + "(:startDate IS NULL AND :endDate IS NOT NULL AND v.date <= :endDate) OR "
-	        + "(:startDate IS NOT NULL AND :endDate IS NULL AND v.date >= :startDate) OR "
-	        + "(:startDate IS NOT NULL AND :endDate IS NOT NULL AND v.date BETWEEN :startDate AND :endDate) OR "
-	        + "(:startDate IS NULL AND :endDate IS NULL)"
-	        + ") "
-			+ "ORDER BY v.orderNumber DESC") // receiveCode를 기준으로 내림차순 정렬
-	Page<ViewPurchaseSummaryEntity> search(@Param("searchField") String searchField, // 검색할 값
-			@Param("codeType") String codeType, // 어떤 코드인지 (입고번호, 발주번호 등)
-			@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, Pageable pageable);
+	@Query("SELECT v FROM ViewPurchaseSummaryEntity v WHERE " + 
+	        "(:searchField IS NULL OR " +
+	        "(:codeType = '발주번호' AND LOWER(CONCAT(v.orderNumber)) LIKE LOWER(CONCAT('%', :searchField, '%'))) OR " + 
+	        "(:codeType = '상품명' AND LOWER(CONCAT(v.productName)) LIKE LOWER(CONCAT('%', :searchField, '%')))) AND " + 
+	        "(" +
+	        "(:startDate IS NULL AND :endDate IS NOT NULL AND v.date <= :endDate) OR " +
+	        "(:startDate IS NOT NULL AND :endDate IS NULL AND v.date >= :startDate) OR " +
+	        "(:startDate IS NOT NULL AND :endDate IS NOT NULL AND v.date BETWEEN :startDate AND :endDate) OR " +
+	        "(:startDate IS NULL AND :endDate IS NULL)" +
+	        ") AND " +
+	        "v.totalWtQuantity > 0 " +
+	        "ORDER BY v.orderNumber DESC")
+	Page<ViewPurchaseSummaryEntity> search(
+	        @Param("searchField") String searchField, 
+	        @Param("codeType") String codeType, 
+	        @Param("startDate") LocalDateTime startDate, 
+	        @Param("endDate") LocalDateTime endDate, 
+	        Pageable pageable);
 }
